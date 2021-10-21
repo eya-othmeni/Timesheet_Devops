2ec175ea1d7dd17ee21ff8745f3a44c18d43a22d
eya.othmeni <57917853+eya-othmeni@users.noreply.github.com>
2021-10-21 13:03:17 +0100

    Create Test class

---
+++ timesheet_devOps/src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -0,0 +1,48 @@
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.spring.entities.Entreprise;
+
+
+public class EntrepriseServiceImplTest {
+	
+	Entreprise ent = new Entreprise();
+	
+	@Test
+	public void testAjouterEntreprise(){
+		
+	}
+
+	@Test
+	public void testAjouterDepartement(){
+		
+	}
+	
+	@Test
+	public void testDeleteEntrepriseById(){
+		
+	}
+	
+	@Test
+	public void testGetAllDepartementsNamesByEntreprise(){
+		
+	}
+	
+	@Test
+	public void testAffecterDepartementAEntreprise(){
+		
+	}
+	
+	
+	@Test
+	public void testDeleteDepartementById(){
+		
+	}
+	
+	@Test
+	public void testGetEntrepriseById(){
+		
+	}
+}

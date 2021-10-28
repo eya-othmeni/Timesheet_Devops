f2c1ca9e1eb5eb7adf22ca47897162aad3c1e362
eya.othmeni <57917853+eya-othmeni@users.noreply.github.com>
2021-10-28 16:01:36 +0100

    Update EntrepriseServiceImplTest.java

--- timesheet_devOps/src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ timesheet_devOps/src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -1,7 +1,8 @@
 package tn.esprit.spring;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,49 +15,59 @@ public class EntrepriseServiceImplTest {
 	
 	@Autowired
 	IEntrepriseService ent;
-	private static final Logger l = LogManager.getLogger(EntrepriseServiceImplTest.class);
 
 	
 	@Test
+	@Order(1)
 	public void testAjouterEntreprise(){
 		
-		Entreprise etrp = new Entreprise("test2", "5");
-		ent.ajouterEntreprise(etrp) ;
+		Entreprise etrp = new Entreprise("test3", "25");
+		Assertions.assertEquals(etrp.getName(),ent.ajouterEntreprise(etrp).getName());
 		
 	}
 
 	@Test
+	@Order(2)
 	public void testAjouterDepartement(){
 		
-		Departement dep =new Departement("ING");
-		ent.ajouterDepartement(dep);
+		Departement dep =new Departement("info");
+		Integer id=ent.ajouterDepartement(dep);
+		Assertions.assertEquals(id, dep.getId());
 	}
 	
 	@Test
+	@Order(3)
 	public void testAffecterDepartementAEntreprise(){
-		ent.affecterDepartementAEntreprise(5, 13);
+		ent.affecterDepartementAEntreprise(16, 5);
 	}
 	
 	@Test
+	@Order(4)
 	public void testGetAllDepartementsNamesByEntreprise(){
 		 ent.getAllDepartementsNamesByEntreprise(3);
+		// Assertions.assertEquals(1, ent.getAllDepartementsNamesByEntreprise(3).size());
 		
 	}
 	
 	@Test
+	@Order(5)
 	public void testDeleteEntrepriseById(){
 		ent.deleteEntrepriseById(12);
+		Assertions.assertNull(ent.getEntrepriseById(12));
 	}
 	
 	@Test
+	@Order(6)
 	public void testDeleteDepartementById(){
 		ent.deleteDepartementById(11);
+		Assertions.assertNull(ent.getDepartementById(11));
 	}
 	
 	@Test
+	@Order(7)
 	public void testGetEntrepriseById(){
 		Entreprise e= ent.getEntrepriseById(5);
+		Assertions.assertEquals(e.getId(),5 );
 		
-		l.info(e);
 	}
 }

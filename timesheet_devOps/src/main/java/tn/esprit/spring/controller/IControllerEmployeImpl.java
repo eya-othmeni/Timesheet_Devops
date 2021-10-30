695613fffe0bf7b7b9c6b61f66e24f64e37d5504
NourElhoudaNasri <78796864+NourElhoudaNasri@users.noreply.github.com>
2021-10-30 20:56:14 +0100

    employee service + controller

---
+++ timesheet_devOps/src/main/java/tn/esprit/spring/controller/IControllerEmployeImpl.java
@@ -0,0 +1,78 @@
+package tn.esprit.spring.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.entities.Timesheet;
+import tn.esprit.spring.services.IEmployeService;
+import tn.esprit.spring.services.IEntrepriseService;
+
+
+@Controller
+public class IControllerEmployeImpl  {
+	@Autowired
+	IEmployeService iemployeservice;
+	
+	
+
+	public int ajouterEmploye(Employe employe)
+	{
+		iemployeservice.ajouterEmploye(employe);
+		return employe.getId();
+	}
+    
+	public void mettreAjourEmailByEmployeId(String email, int employeId) {
+		iemployeservice.mettreAjourEmailByEmployeId(email, employeId);
+		
+	}
+
+	public void affecterEmployeADepartement(int employeId, int depId) {
+		iemployeservice.affecterEmployeADepartement(employeId, depId);
+		
+	}
+
+
+	
+	public void desaffecterEmployeDuDepartement(int employeId, int depId)
+	{
+		iemployeservice.desaffecterEmployeDuDepartement(employeId, depId);
+	}
+
+	
+	public int ajouterContrat(Contrat contrat) {
+		iemployeservice.ajouterContrat(contrat);
+		return contrat.getReference();
+	}
+	
+	public void affecterContratAEmploye(int contratId, int employeId)
+	{
+		iemployeservice.affecterContratAEmploye(contratId, employeId);
+	}
+
+	
+	public String getEmployePrenomById(int employeId) {
+		return iemployeservice.getEmployePrenomById(employeId);
+	}
+
+	
+	public void deleteEmployeById(int employeId) {
+		iemployeservice.deleteEmployeById(employeId);
+		
+	}
+	public void deleteContratById(int contratId) {
+		iemployeservice.deleteContratById(contratId);
+	}
+
+	
+
+	
+	
+	
+}
